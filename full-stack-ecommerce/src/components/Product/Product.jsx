b0eba937e0c95d14e4e63627a458679a087c50a7
samlarson94 <samlarson94@gmail.com>
2022-07-14 10:03:40 -0500

    Building out Product card, adding props to Products/Product components.

--- full-stack-ecommerce/src/components/Product/Product.jsx
+++ full-stack-ecommerce/src/components/Product/Product.jsx
@@ -3,11 +3,31 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from
 import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
 import { mergeClasses } from '@material-ui/styles';
 
-const Product = () => {
+const Product = ({ product }) => {
   return (
     <div>
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' />
+            <CardMedia className={classes.media} image='' title={product.name}/>
+            <CardContent>
+                <div className="classes.cardContent">
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5" >
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant="h2" color="textSecondary">
+                    {product.description}
+                </Typography>
+            </CardContent>
+
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
+
 
         </Card>
     </div>
--- full-stack-ecommerce/src/components/Products.jsx
+++ full-stack-ecommerce/src/components/Products.jsx
@@ -3,10 +3,11 @@ import Grid from '@material-ui/core';
 
 //Produts array of objects
     // id, name, description, price, stock
+        //Pass as prop to individual Product below
 const products = [
-    {id: 1, name: "Headphones", description: "Wireless headphones" },
-    {id: 2, name: "iPod Touch", description: "Consumer electronics." },
-    {id: 3, name: "RayBans", description: "Swag." }
+    {id: 1, name: "Headphones", description: "Wireless headphones", price: "$50.00" },
+    {id: 2, name: "iPod Touch", description: "Consumer electronics.", price: "$100.00" },
+    {id: 3, name: "RayBans", description: "Swag.", price: "$117.50" }
 ];
 
 const Products = () => {
@@ -15,7 +16,7 @@ const Products = () => {
     <Grid container justify="center" spacing={4}>
     {/* MOCK PRODUCTS */}
     <Grid item key={product.id} xs={12} sm={6} lg={3}>
-        <Product />
+        <Product product={product} />
     </Grid>
     {/* FETCH PRODUCTS FROM API */}
     </Grid>
